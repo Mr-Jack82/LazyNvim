47a8aca6cafd177743c5952a1df4bb3ff9d3a768
Виталий Шевченко <blackjack358@gmail.com>
2023-02-23 22:43:30 +0300

    Disable some plugins

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,7 @@
+-- disable some plugins
+return {
+  { "ggandor/leap.nvim", enabled = false },
+  { "ggandor/flit.nvim", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+}
