3ed2a153617749a45490bb56b5895a6cd14fc5b2
Виталий Шевченко <blackjack358@gmail.com>
2023-02-23 22:44:35 +0300

    Add custom options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
+vim.g.mapleader = ","
+
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+
+opt.scrolloff = 6
+opt.textwidth = 80
+opt.colorcolumn = "81"
+opt.softtabstop = 2
