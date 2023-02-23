71fb97c2c5e9beca886db1443379516c7052b2de
Виталий Шевченко <blackjack358@gmail.com>
2023-02-23 22:38:24 +0300

    Update nvim-cmp config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -29,8 +29,8 @@ return {
         end,
       },
       window = {
-        completion = { border = "single" },
-        documentation = { border = "single" },
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
       },
       mapping = cmp.mapping.preset.insert({
         ["<C-p>"] = cmp.mapping.select_prev_item(),
