e3b45bb5d8df5441b5d92f7d4a88c1cb201a7c5d
Виталий Шевченко <blackjack358@gmail.com>
2023-03-15 01:26:52 +0300

    Add an option to 'cmdline' plugin

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -54,8 +54,8 @@ return {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-          -- they way you will only jump inside the snippet region
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+            -- they way you will only jump inside the snippet region
           elseif luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
           elseif has_words_before() then
@@ -107,7 +107,12 @@ return {
         sources = cmp.config.sources({
           { name = "path" },
         }, {
-          { name = "cmdline" },
+          {
+            name = "cmdline",
+            option = {
+              ignore_cmds = { "Man", "!" },
+            },
+          },
         }),
       })
 
