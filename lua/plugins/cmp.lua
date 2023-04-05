85caf4fa925274863bbaabf77ffc84b70304be2a
Виталий Шевченко <blackjack358@gmail.com>
2023-04-05 15:16:35 +0300

    Cmp: don't use frame on autocompletion list

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -36,7 +36,7 @@ return {
       }
 
       opts.window = {
-        completion = cmp.config.window.bordered(),
+        -- completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
       }
 
