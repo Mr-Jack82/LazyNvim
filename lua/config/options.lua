d2035c74db9a7dfd09e09f40b068d8222b348593
Виталий Шевченко <blackjack358@gmail.com>
2023-12-02 19:01:11 +0300

    Use refined 'cmdheight' option

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,6 +8,9 @@ local opt = vim.opt
 opt.scrolloff = 6
 opt.textwidth = 80
 opt.colorcolumn = "81"
-opt.cmdheight = 0
 opt.softtabstop = 2
 opt.ttimeoutlen = 0
+
+if vim.fn.has("nvim-0.8") == 1 then
+  opt.cmdheight = 0
+end
