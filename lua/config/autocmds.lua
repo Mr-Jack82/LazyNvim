f238c119417f59a01c15d55790ddd3c8e136ccfd
Виталий Шевченко <blackjack358@gmail.com>
2024-01-01 00:58:04 +0300

    Autocmd: delete trailing white space

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -23,3 +23,36 @@ autocmd("BufWritePost", {
   group = "mygroup",
   desc = "Reload config on save",
 })
+
+-- Source: https://github.com/mcauley-penney/tidy.nvim
+autocmd("BufWritePre", {
+  pattern = "*",
+  desc = "Delete trailing white space",
+  group = augroup("TrimTrailingSpace", { clear = true }),
+  callback = function()
+    local pos = vim.api.nvim_win_get_cursor(0)
+
+    -- delete trailing whitespace
+    vim.cmd([[:keepjumps keeppatterns %s/\s\+$//e]])
+
+    -- delete lines @ eof
+    vim.cmd([[:keepjumps keeppatterns silent! 0;/^\%(\n*.\)\@!/,$d_]])
+
+    local num_rows = vim.api.nvim_buf_line_count(0)
+
+    --[[
+            if the row value in the original cursor
+            position tuple is greater than the
+            line count after empty line deletion
+            (meaning that the cursor was inside of
+            the group of empty lines at the end of
+            the file when they were deleted), set
+            the cursor row to the last line.
+        ]]
+    if pos[1] > num_rows then
+      pos[1] = num_rows
+    end
+
+    vim.api.nvim_win_set_cursor(0, pos)
+  end,
+})
