d20d6bc84271250a1ec2ca6f960d98aa1d223a2c
Виталий Шевченко <blackjack358@gmail.com>
2024-01-01 01:06:21 +0300

    Options: add a few useful options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,6 +11,17 @@ opt.colorcolumn = "81"
 opt.softtabstop = 2
 opt.ttimeoutlen = 0
 opt.path:append(".**")
+opt.swapfile = false
+opt.wildignorecase = true
+opt.wildoptions = "pum,fuzzy"
+opt.diffopt:append({
+  "vertical",
+  "iwhite",
+  "hiddenoff",
+  "algorithm:histogram",
+  "indent-heuristic",
+  "linematch:60",
+})
 
 if vim.fn.has("nvim-0.8") == 1 then
   opt.cmdheight = 0
