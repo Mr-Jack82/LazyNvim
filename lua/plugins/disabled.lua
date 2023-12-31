341566d1f48184881c21fe2862b9bd855b6790f0
Виталий Шевченко <blackjack358@gmail.com>
2024-01-01 00:55:32 +0300

    Plugins: move from 'mini.surround' to 'nvim-surround'

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -6,4 +6,5 @@ return {
   { "rcarriga/nvim-notify", enabled = false },
   { "folke/noice.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "echasnovski/mini.surround", enabled = false },
 }
